e857eb80939e5d5da2a71f867c0ded1c77aea5ac
Meezaan Davids <meezaandavids365@gmail.com>
2026-06-09 11:01:20 +0200

    chore: removed flash and netlify file

--- flash/src/types/product.ts
+++
@@ -1,9 +0,0 @@
-export type Product = {
-	id: string;
-	name: string;
-	slug: string;
-	price: number;
-	category: string;
-	image: string;
-	description: string;
-};
--- flash/src/utils/slugify.ts
+++
@@ -1,7 +0,0 @@
-export function slugify(value: string): string {
-	return value
-		.toLowerCase()
-		.trim()
-		.replace(/[^\w\s-]/g, "")
-		.replace(/\s+/g, "-")
-}
